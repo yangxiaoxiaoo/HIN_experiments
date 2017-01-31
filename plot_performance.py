40439c2ee5e34248f20f1f82c17e2a446ac34eb0
yangxiaoxiaoo <yangxiaoxiaoo@163.com>
2017-01-30 21:05:20 -0500

    Plotting script updated

--- plot_performance.py
+++ plot_performance.py
@@ -4,43 +4,54 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-def plot(AlgoMeans, AlgoStd, baseMeans, baseStd, title, unit):
+def plot(AlgoMeans, AlgoStd, baseMeans, baseStd, backboneMeans, backboneStd, title, unit):
 
     N = len(AlgoMeans)               # number of data entries
     ind = np.arange(N)              # the x locations for the groups
-    width = 0.35                    # bar width
+    width = 0.25                    # bar width
 
     fig, ax = plt.subplots()
 
     rects1 = ax.bar(ind, AlgoMeans,                  # data
                     width,                          # bar width
                     color='MediumSlateBlue',        # bar colour
-                    yerr=AlgoStd,                  # data for error bars
+             #       log = True,
+             #       yerr=AlgoStd,                  # data for error bars
                     error_kw={'ecolor':'Tomato',    # error-bars colour
                               'linewidth':2})       # error-bar width
 
     rects2 = ax.bar(ind + width, baseMeans,
                     width,
                     color='Tomato',
-                    yerr=baseStd,
+               #     log = True,
+               #     yerr=baseStd,
                     error_kw={'ecolor':'MediumSlateBlue',
                               'linewidth':2})
 
+    rects3 = ax.bar(ind +  2*width, backboneMeans,
+                    width,
+                    color='Green',
+             #       log = True,                            setting log true will be bad for showing significance
+              #      yerr=backboneStd,
+                    error_kw={'ecolor':'Green',
+                              'linewidth':2})
+
     axes = plt.gca()
- #   axes.set_ylim([0, 41])             # y-axis bounds
 
 
     ax.set_ylabel(unit)
     ax.set_title(title)
     ax.set_xticks(ind + width)
     ax.set_xticklabels(('shape1', 'shape2','shape5', 'shape6', 'shape7'))
-    ax.set_yscale('log')
-    ax.legend((rects1[0], rects2[0]), ('Algorithm', 'Baseline'))
+
+ #   ax.set_ylim([100, 0.1])
+
+    ax.legend((rects1[0], rects2[0], rects3[0]), ('Algorithm', 'Bruteforce', 'Backbone'))
 
     def autolabel(rects):
         for rect in rects:
             height = rect.get_height()
-            ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+            ax.text(rect.get_x() + rect.get_width()/3., 1.05*height,
                     '%.2f' % float(height),
                     ha='center',            # vertical alignment
                     va='bottom'             # horizontal alignment
@@ -48,11 +59,12 @@ def plot(AlgoMeans, AlgoStd, baseMeans, baseStd, title, unit):
 
     autolabel(rects1)
     autolabel(rects2)
+    autolabel(rects3)
 
     plt.show()
 
 
-def stat_time(algoMeans, algoStd, baselineMeans, baselineStd, shape, instring):
+def stat(mode, algoMeans, algoStd, baselineMeans, baselineStd, backboneMeans, backboneStd, shape, instring):
     algo_times = list()
     algo_mem = list()
     algo_space = list()
@@ -61,160 +73,113 @@ def stat_time(algoMeans, algoStd, baselineMeans, baselineStd, shape, instring):
     baseline_mem = list()
     baseline_space = list()
 
-    onlyfiles = [f for f in listdir(instring+"/output"+str(shape)) if isfile(join(instring+"/output"+str(shape), f))]
-    for file in onlyfiles:
-        with open(join(instring+"/output"+str(shape), file), "r") as fp:
-            counter = 0
-            for line in fp:
-                if counter == 0:
-                    algo_times.append(float(line.split('\t')[0]))
-                    print float(line.split('\t')[0])
-                    algo_mem.append(float(line.split('\t')[2]))
-                    algo_space.append(float(line.split('\t')[3]))
-
-                    counter+=1
-                else:
-                    baseline_times.append(float(line.split('\t')[0]))
-                    baseline_mem.append(float(line.split('\t')[2]))
-                    baseline_space.append(float(line.split('\t')[3]))
-
-    algoMeans.append(sum(algo_times)/len(algo_times))
-    algoStd.append(math.sqrt(np.var(algo_times)))
-    baselineMeans.append(sum(baseline_times)/len(baseline_times))
-    baselineStd.append(math.sqrt(np.var(baseline_times)))
-
-    print algoMeans
-    print baselineMeans
-
-    return (algoMeans, algoStd, baselineMeans, baselineStd)
-
-def stat_mem(algoMeans, algoStd, baselineMeans, baselineStd, shape, instring):
-    algo_times = list()
-    algo_mem = list()
-    algo_space = list()
-
-    baseline_times = list()
-    baseline_mem = list()
-    baseline_space = list()
+    backbone_times = list()
+    backbone_mem = list()
+    backbone_space = list()
 
     onlyfiles = [f for f in listdir(instring+"/output"+str(shape)) if isfile(join(instring+"/output"+str(shape), f))]
     for file in onlyfiles:
         with open(join(instring+"/output"+str(shape), file), "r") as fp:
-            counter = 0
-            for line in fp:
+
+            for counter, line in enumerate(fp):
                 if counter == 0:
                     algo_times.append(float(line.split('\t')[0]))
-                    print float(line.split('\t')[0])
                     algo_mem.append(float(line.split('\t')[2]))
                     algo_space.append(float(line.split('\t')[3]))
 
-                    counter+=1
-                else:
+                    print line
+                if counter == 1:
                     baseline_times.append(float(line.split('\t')[0]))
                     baseline_mem.append(float(line.split('\t')[2]))
                     baseline_space.append(float(line.split('\t')[3]))
 
-    algoMeans.append(sum(algo_mem)/len(algo_mem))
-    algoStd.append(math.sqrt(np.var(algo_mem)))
-    baselineMeans.append(sum(baseline_mem)/len(baseline_mem))
-    baselineStd.append(math.sqrt(np.var(baseline_mem)))
+                    print line
+                if counter == 2:
+                    backbone_times.append(float(line.split('\t')[0]))
+                    backbone_mem.append(float(line.split('\t')[2]))
+                    backbone_space.append(float(line.split('\t')[3]))
+                    print "a three line file !"
+
+   # print str(backbone_times == baseline_times) +"equal "
+
+    if mode == 'time':
+
+        algoMeans.append(sum(algo_times)/len(algo_times))
+        algoStd.append(math.sqrt(np.var(algo_times)))
+        baselineMeans.append(sum(baseline_times)/len(baseline_times))
+        baselineStd.append(math.sqrt(np.var(baseline_times)))
+        backboneMeans.append(sum(backbone_times)/len(backbone_times))
+        backboneStd.append(math.sqrt(np.var(backbone_times)))
+
+    if mode == "mem":
+        algoMeans.append(sum(algo_mem)/len(algo_mem))
+        algoStd.append(math.sqrt(np.var(algo_mem)))
+        baselineMeans.append(sum(baseline_mem)/len(baseline_mem))
+        baselineStd.append(math.sqrt(np.var(baseline_mem)))
+        backboneMeans.append(sum(backbone_mem)/len(backbone_mem))
+        backboneStd.append(math.sqrt(np.var(backbone_mem)))
+
+    if mode == "space":
+        algoMeans.append(sum(algo_space)/len(algo_space))
+        algoStd.append(math.sqrt(np.var(algo_space)))
+        baselineMeans.append(sum(baseline_space)/len(baseline_space))
+        baselineStd.append(math.sqrt(np.var(baseline_space)))
+        backboneMeans.append(sum(backbone_mem)/len(backbone_space))
+        backboneStd.append(math.sqrt(np.var(backbone_space)))
+
 
     print algoMeans
     print baselineMeans
 
-    return (algoMeans, algoStd, baselineMeans, baselineStd)
-
-
-def stat_space(algoMeans, algoStd, baselineMeans, baselineStd, shape, instring):
-    algo_times = list()
-    algo_mem = list()
-    algo_space = list()
-
-    baseline_times = list()
-    baseline_mem = list()
-    baseline_space = list()
-
-    onlyfiles = [f for f in listdir(instring+"/output"+str(shape)) if isfile(join(instring+"/output"+str(shape), f))]
-    for file in onlyfiles:
-        with open(join(instring+"/output"+str(shape), file), "r") as fp:
-            counter = 0
-            for line in fp:
-                if counter == 0:
-                    algo_times.append(float(line.split('\t')[0]))
-                    print float(line.split('\t')[0])
-                    algo_mem.append(float(line.split('\t')[2]))
-                    algo_space.append(float(line.split('\t')[3]))
-
-                    counter+=1
-                else:
-                    baseline_times.append(float(line.split('\t')[0]))
-                    baseline_mem.append(float(line.split('\t')[2]))
-                    baseline_space.append(float(line.split('\t')[3]))
-
-    algoMeans.append(sum(algo_space)/len(algo_space))
-    algoStd.append(math.sqrt(np.var(algo_space)))
-    baselineMeans.append(sum(baseline_space)/len(baseline_space))
-    baselineStd.append(math.sqrt(np.var(baseline_space)))
-
-    print algoMeans
-    print baselineMeans
-
-    return (algoMeans, algoStd, baselineMeans, baselineStd)
+    return (algoMeans, algoStd, baselineMeans, baselineStd, backboneMeans, backboneStd)
 
 
 
 
-def plottime(dataset, instring, shapes):
+def compare(mode, dataset, instring, shapes):
+
+
     algoMeans = list()
     algoStd = list()
     baselineMeans = list()
     baselineStd = list()
+    backboneMeans = list()
+    backboneStd = list()
 
     for shape in shapes:
         print shape
-        algoMeans, algoStd, baselineMeans, baselineStd = stat_time(algoMeans, algoStd, baselineMeans, baselineStd, shape, instring)
-
-    plot(algoMeans, algoStd, baselineMeans, baselineStd, "Running time for shapes of queries:"+dataset, 'Time:/s')
-
-
-
-def plotmem(dataset, instring, shapes):
-    algoMeans = list()
-    algoStd = list()
-    baselineMeans = list()
-    baselineStd = list()
-
-
-    for shape in shapes:
-        algoMeans, algoStd, baselineMeans, baselineStd = stat_mem(algoMeans, algoStd, baselineMeans, baselineStd, shape, instring)
-
-    plot(algoMeans, algoStd, baselineMeans, baselineStd, "Memory usage for shapes of queries: "+dataset, 'numbers')
-
-
-def plotspace(dataset, instring, shapes):
-    algoMeans = list()
-    algoStd = list()
-    baselineMeans = list()
-    baselineStd = list()
-
-    for shape in shapes:
-        algoMeans, algoStd, baselineMeans, baselineStd =  stat_space(algoMeans, algoStd, baselineMeans, baselineStd, shape, instring)
-
-    plot(algoMeans, algoStd, baselineMeans, baselineStd, "Total search space for shapes of queries:"+dataset, 'numbers')
-
-
+        algoMeans, algoStd, baselineMeans, baselineStd, backboneMeans, backboneStd = stat(mode, algoMeans, algoStd, baselineMeans, baselineStd,  backboneMeans, backboneStd, shape, instring)
+    if mode == 'time':
+        plot(algoMeans, algoStd, baselineMeans, baselineStd,  backboneMeans, backboneStd, "Running time for shapes of queries:"+dataset, 'Time:/s')
+    if mode == 'mem':
+        plot(algoMeans, algoStd, baselineMeans, baselineStd,  backboneMeans, backboneStd, "Memory usage for shapes of queries: "+dataset, 'counts')
+    if mode == 'space':
+        plot(algoMeans, algoStd, baselineMeans, baselineStd,  backboneMeans, backboneStd, "Total search space for shapes of queries:"+dataset, 'counts')
 
 
 
 
 def main():
-    dataset = 'PhotoNet'
-    instring = dataset + 'outputs'
-    shapes = [1, 2, 5, 6, 7]
 
-    plottime(dataset, instring, shapes)
-    plotmem(dataset, instring, shapes)
-    plotspace(dataset, instring, shapes)
+    dataset = 'Enron'
+    instring = dataset + 'outputs'
+    shapes = [1, 2, 6]
+ #   measurement = ['time', 'mem', 'space']  memory and space definition need to be updated
+    measurement = ['time']
+    for item in measurement:
+        compare(item, dataset, instring, shapes)
+
+
+#Enron has only query template 1, 2, 6En
+    datasets = ['PhotoNet', 'DBLP', 'YelpPhoto']
+    for dataset in datasets:
+        instring = dataset + 'outputs'
+        shapes = [1, 2, 5, 6, 7]
+
+     #   measurement = ['time', 'mem', 'space']  memory and space definition need to be updated
+        measurement = ['time']
+        for item in measurement:
+            compare(item, dataset, instring, shapes)
 
 
 if __name__ == "__main__":
